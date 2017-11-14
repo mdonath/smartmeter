b3024d9ca7d3fd3ca02d32561a443d1a95595b08
Martijn Donath <martijn.donath@gmail.com>
2017-11-14 20:19:08 +0100

    including time every 10 seconds

--- mqtt/display_segment.py
+++ mqtt/display_segment.py
@@ -2,7 +2,7 @@
 
 from Adafruit_LED_Backpack import SevenSegment
 import paho.mqtt.client as mqtt
-import time
+import time, datetime
 
 #
 #    0+     16+     48+     64+
@@ -12,7 +12,11 @@ import time
 #  4   2   4   2   4   2   4   2
 #  *-3-*   *-3-*   *-3-*   *-3-*
 #
-
+#    01
+#  32  02
+#    64
+#  16  04
+#    08
 
 ENERGY_TOPIC = "home/energy/power"
 MQTT_HOST = "mqtt"
@@ -34,6 +38,7 @@ class MqttDisplay(object):
 		self.display = SevenSegment.SevenSegment()
 		self.display.begin()
 		self.display_started()
+		self.display.set_brightness(1)
 
 		self.mqttc = mqtt.Client("display_7segment")
 		self.mqttc.on_message = callback
@@ -41,31 +46,78 @@ class MqttDisplay(object):
 		self.mqttc.subscribe(topic)
 		self.display_connected()
 
+		self.showMQTT = False
+		self.showTime = True
+
 
 	def run_forever(self):
-		self.mqttc.loop_forever()
+		# self.mqttc.loop_forever()
+		self.mqttc.loop_start()
 
 
 	def display_payload(self, message):
 		payload = str(message.payload.decode("utf-8"))
+		self.display_string(payload)
+
+	def display_string(self, payload):
 		if payload == '----':
 			self.display_error()
-		else:
-			self.display.print_number_str(payload)
+		elif self.showTime == True:
+			self.showTime = True
+			self.display.set_left_colon(False)
+
+			now = datetime.datetime.now()
+			hour = now.hour
+			minute = now.minute
+			second = now.second
+
+			self.display.clear()
+			# Set hours
+			self.display.set_digit(0, int(hour / 10))     # Tens
+			self.display.set_digit(1, hour % 10)          # Ones
+			# Set minutes
+			self.display.set_digit(2, int(minute / 10))   # Tens
+			self.display.set_digit(3, minute % 10)        # Ones
+			# Toggle colon
+			self.display.set_colon(second % 2)            # Toggle colon at 1Hz
+			
+			# Write the display buffer to the hardware.  This must be called to
+			# update the actual display LEDs.
+			self.display.write_display()
+
+		elif self.showMQTT:
+			self.display.clear()
+			self.display.print_float(float(payload), decimal_digits=3, justify_right=False)
 			self.colon = not self.colon
+			self.display.set_colon(False)
 			self.display.set_left_colon(self.colon)
 			self.display.write_display()
 
+		else:
+			self.display_leds_mask( [83,83,83,83] )
+
+#       01(0)
+#  32(5)     02(1)
+#       64(6)
+#  16(4)     04(2)
+#       08(3)
 
 	def display_started(self):
-		self.display_leds( [6, 16+6, 48+6, 64+6] )
+		self.display_leds_mask( [64,64,64,64] )
 	
 	def display_connected(self):
-		self.display_leds( [3,4,6, 16+2,16+3,16+4,16+6, 48+2,48+4,48+6, 64+2,64+4,64+6] )
+		self.display_leds_mask( [88,92,84,84] )
 
 	def display_error(self):
 		self.display_leds([0,4,5,6,  16+0,16+1,16+2,16+3,16+4,16+5,  48+1,48+2,48+3,48+4,48+5, 64+3,64+4,64+5,64+6])
 
+	def display_leds_mask(self, leds):
+		self.display.clear()
+		for i, mask in enumerate(leds):
+			self.display.set_digit_raw(i,mask)
+		self.display.write_display()
+		time.sleep(1)
+	
 	def display_leds(self, leds):
 		self.display.clear()
 		for led in leds:
@@ -76,4 +128,9 @@ class MqttDisplay(object):
 if __name__ == "__main__":
 	md = MqttDisplay(on_message)
 	md.run_forever()
+	print("loop started")
+	while True:
+		time.sleep(3 if md.showTime else 10)
+		md.showTime = not md.showTime
+		md.showMQTT = not md.showMQTT
 
